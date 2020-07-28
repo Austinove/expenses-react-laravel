021935d03643fdff599af5dc041a5601fd02da10
Austinove <bryanovicaustenove@gmail.com>
2020-07-28 11:28:24 +0300

    action constants

---
+++ resources/js/containers/constants/index.js
@@ -0,0 +1,54 @@
+//API Url
+export const ApiUrl = "http://localhost:8000";
+//User Constants
+export const userConstants = {
+    fetch_users: "fetch_users_request",
+    fetch_users_success: "fetch_users_successfull",
+    fetch_users_fail: "fetch_users_failure",
+    
+    register_user: "register_user_request",
+    register_user_success: "register_user_successfull",
+    register_user_fail: "register_user_failure",
+
+    edit_user: "edit_user_request",
+    edit_user_success: "edit_user_successfull",
+    edit_user_fail: "edit_user_failure",
+
+    remove_user: "remove_user_request",
+    remove_user_success: "remove_user_successfull",
+    remove_user_fail: "remove_user_failure",
+
+    accept_user: "accept_user_request",
+    accept_user_success: "accept_user_successfull",
+    accept_user_fail: "accept_user_failure",
+
+    login_user: "login_user_request",
+    login_user_success: "login_user_successfull",
+    login_user_fail: "login_user_failure",
+}
+//ExpensesConstants
+export const expensesConstants = {
+    fetch_expenses: "fetch_expenses_request",
+    fetch_expenses_success: "fetch_expenses_successfull",
+    fetch_expenses_fail: "fetch_expenses_failure",
+
+    create_expense: "create_expense",
+    create_expense_success: "create_expense_successfull",
+    create_expense_fail: "create_expense_failure",
+
+    edit_expense: "edit_expense",
+    edit_expense_success: "edit_expense_successfull",
+    edit_expense_fail: "edit_expense_failure",
+
+    delete_expense: "delete_expense",
+    delete_expense_success: "delete_expense_successfull",
+    delete_expense_fail: "delete_expense_failure",
+
+    accept_expense: "accept_expense",
+    accept_expense_success: "accept_expense_successfull",
+    accept_expense_fail: "accept_expense_failure",
+
+    decline_expense: "decline_expense",
+    decline_expense_success: "decline_expense_successfull",
+    decline_expense_fail: "decline_expense_failure",
+}
